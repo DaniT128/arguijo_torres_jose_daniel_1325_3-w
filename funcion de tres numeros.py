c8b0b5f270bedf163002a33261673d6cd1e8a5ad
DaniT128 <a.23308051281325@cbtis128.edu.mx>
2024-10-09 00:09:43 -0600

    Add files via upload

---
+++ funcion de tres numeros.py
@@ -0,0 +1,15 @@
+(" ")
+("jose daniel arguijo torres_1325-3-w")
+(" ")
+
+def mayor_de_tres(num1, num2, num3):
+    """Devuelve el mayor de tres números."""
+    return max(num1, num2, num3)
+
+# Ejemplo de uso
+numero1 = 15
+numero2 = 50
+numero3 = 35
+
+mayor = mayor_de_tres(numero1, numero2, numero3)
+print(f"El mayor de los tres números es: {mayor}")
