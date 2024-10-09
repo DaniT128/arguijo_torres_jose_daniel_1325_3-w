d3e32eca03cab6bcb062d5b508d05ce156f44414
DaniT128 <a.23308051281325@cbtis128.edu.mx>
2024-10-08 23:57:42 -0600

    Add files via upload

---
+++ direccion emil.py
@@ -0,0 +1,19 @@
+(" ")
+("jose daniel arguijo torres_1325_3-w")
+(" ")
+
+def es_email_valido(email):
+    """Verifica si el email contiene el símbolo '@'."""
+    return '@' in email
+
+def capturar_email():
+    """Captura una dirección de email y verifica su validez."""
+    email = input("Por favor, ingresa tu dirección de email: ")
+    
+    if es_email_valido(email):
+        print("La dirección de email es válida.")
+    else:
+        print("La dirección de email no es válida.")
+
+# Ejemplo de uso
+capturar_email()
