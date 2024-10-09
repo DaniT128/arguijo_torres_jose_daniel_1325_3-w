6ea027d6ddc78688cdfa067eee53cc0479c0445b
DaniT128 <a.23308051281325@cbtis128.edu.mx>
2024-10-08 23:33:56 -0600

    Add files via upload

---
+++ saludo.py
@@ -0,0 +1,13 @@
+(" ")
+("jose daniel arguijo torres_1325_3-w")
+(" ")
+
+def saludar(nombre):
+    if nombre:
+        print(f"¡Hola {nombre}!")
+    else:
+        print("Por favor, ingresa un nombre válido.")
+
+# Ejemplo de uso
+saludar("Ana")
+saludar("")  # Prueba con un nombre vacío
