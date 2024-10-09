aab8f5a60cc23f5292dbc0ea68520944cbffb959
DaniT128 <a.23308051281325@cbtis128.edu.mx>
2024-10-09 00:21:42 -0600

    Add files via upload

---
+++ funcion de carater.py
@@ -0,0 +1,14 @@
+(" ")
+("jose daniel arguijo torres_1325_3-w")
+(" ")
+
+def es_vocal(caracter):
+    """Devuelve True si el carácter es una vocal, de lo contrario False."""
+    return caracter.lower() in 'aeiou'
+
+# Ejemplo de uso
+caracter = input("Ingresa un carácter: ")
+if es_vocal(caracter):
+    print(f"{caracter} es una vocal.")
+else:
+    print(f"{caracter} no es una vocal.")
